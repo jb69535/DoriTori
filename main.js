d97835bcf7c380e195a9b88f3a14f8376e9912e3
jb69535 <92282867+jb69535@users.noreply.github.com>
2022-08-27 23:01:04 -0400

    HTML + CSS + JS: Navbar Scroll, Produts

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+'use strict';
+/* Navbar
+Scrolling acts */
+// Make navbar trasparent when it is on the top.
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener(
+    'scroll', () => {
+        console.log(window.scrollY)
+        console.log('navbarHeight: ${navbarHeight}');
+        if (window.scrollY > navbarHeight) {
+            navbar.classList.add('navbar--dark');
+        } else {
+            navbar.classList.remove('navbar--dark');
+        }
+    });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    navbarMenu.classList.remove('open');
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+});
 /* Ads
 Makes Ads move automatically every 5s */ 
 var counter = 1;
